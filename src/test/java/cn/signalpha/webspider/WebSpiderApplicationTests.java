90c14056a4425d34a07b1ab143f4705b34c65a7d
suguangjun <signalpha@qq.com>
2018-11-28 17:30:47 +0800

    web 爬虫

---
+++ src/test/java/cn/signalpha/webspider/WebSpiderApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.signalpha.webspider;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WebSpiderApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
